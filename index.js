b02bb4d460848decf17fff9ffdd3bad6fd5d3b6a
John O'Sullivan <john@eximchain.com>
2019-03-08 15:59:47 -0500

    Updated constructor to share one web3 instance rather than create two; patch vbump

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const Web3 = require('web3');
 const WeylABI = require('./weylABI').default;
 const bvABI = require('./bvABI').default;
 
-const buildWeylClient = (url) => {
-    const web3 = new Web3(new Web3.providers.HttpProvider(url));
-    return new web3.eth.Contract(WeylABI, '0x000000000000000000000000000000000000002A');
-}
-
-const buildBlockmakerChecker = (url) => {
-    const web3 = new Web3(new Web3.providers.HttpProvider(url));
+const buildBlockmakerChecker = (web3) => {
     const BV = new web3.eth.Contract(bvABI, '0x0000000000000000000000000000000000000020')
     return (address) => {
         return BV.methods.isBlockMaker(address).call()
@@ -16,8 +10,9 @@ const buildBlockmakerChecker = (url) => {
 }
 
 module.exports.default = (url) => {
+    const web3 = new Web3(new Web3.providers.HttpProvider(url));
     return {
-        contract : buildWeylClient(url),
-        isBlockMaker : buildBlockmakerChecker(url)
+        contract : new web3.eth.Contract(WeylABI, '0x000000000000000000000000000000000000002A'),
+        isBlockMaker : buildBlockmakerChecker(web3)
     }
 };
