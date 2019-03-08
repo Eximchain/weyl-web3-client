a2c315dcef78b86c31a08d46b4f83d5c10c9480b
John O'Sullivan <john@eximchain.com>
2019-03-08 16:06:39 -0500

    Stylistic implementation change; patch vbump

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const Web3 = require('web3');
-const WeylABI = require('./weylABI').default;
-const bvABI = require('./bvABI').default;
-
-const buildBlockmakerChecker = (web3) => {
-    const BV = new web3.eth.Contract(bvABI, '0x0000000000000000000000000000000000000020')
-    return (address) => {
-        return BV.methods.isBlockMaker(address).call()
-    }
-}
 
 module.exports.default = (url) => {
     const web3 = new Web3(new Web3.providers.HttpProvider(url));
+    const BV = new web3.eth.Contract(require('./bvABI').default, '0x0000000000000000000000000000000000000020')
     return {
-        contract : new web3.eth.Contract(WeylABI, '0x000000000000000000000000000000000000002A'),
-        isBlockMaker : buildBlockmakerChecker(web3)
+        contract : new web3.eth.Contract(require('./weylABI').default, '0x000000000000000000000000000000000000002A'),
+        isBlockMaker : (address) => { return BV.methods.isBlockMaker(address).call() }
     }
 };
