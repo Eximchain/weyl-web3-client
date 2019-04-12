773958d8bccaa5850b17681abd64b335d070341c
John O'Sullivan <john@eximchain.com>
2019-04-12 14:08:23 -0400

    Ensured ABIs ended up in build; npm patch vbump

--- build/index.js
+++ build/index.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var web3_1 = __importDefault(require("web3"));
+var weylABI_1 = __importDefault(require("./weylABI"));
+var bvABI_1 = __importDefault(require("./bvABI"));
 exports.buildClient = function (url) {
     var web3 = new web3_1["default"](new web3_1["default"].providers.HttpProvider(url));
-    var BV = new web3.eth.Contract(require('./bvABI')["default"], '0x0000000000000000000000000000000000000020');
+    var BV = new web3.eth.Contract(bvABI_1["default"], '0x0000000000000000000000000000000000000020');
     return {
-        contract: new web3.eth.Contract(require('./weylABI')["default"], '0x000000000000000000000000000000000000002A'),
+        contract: new web3.eth.Contract(weylABI_1["default"], '0x000000000000000000000000000000000000002A'),
         isBlockMaker: function (address) { return BV.methods.isBlockMaker(address).call(); }
     };
 };
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import Web3 from 'web3';
 import { Contract } from 'web3-eth-contract';
+import WeylABI from './weylABI';
+import BlockVoteABI from './bvABI';
 
 export type isBlockMaker = (address:string)=>Promise<boolean>
 
@@ -12,9 +14,9 @@ export type buildClient = (url:string) => Client
 
 export const buildClient:buildClient = (url) => {
     const web3 = new Web3(new Web3.providers.HttpProvider(url));
-    const BV = new web3.eth.Contract(require('./bvABI').default, '0x0000000000000000000000000000000000000020')
+    const BV = new web3.eth.Contract(BlockVoteABI, '0x0000000000000000000000000000000000000020')
     return {
-        contract : new web3.eth.Contract(require('./weylABI').default, '0x000000000000000000000000000000000000002A'),
+        contract : new web3.eth.Contract(WeylABI, '0x000000000000000000000000000000000000002A'),
         isBlockMaker : (address) => { return BV.methods.isBlockMaker(address).call() }
     }
 };
